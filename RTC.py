4ffba7815749d7a149a709bae648db2454ebff10
Anirudh Swarankar <swarnakar.ani24@gmail.com>
2018-03-08 13:08:38 +0530

    Update RTC.py

--- RTC.py
+++ RTC.py
@@ -1,16 +1,21 @@
 import os
 import time
-
+from datetime import datetime
 def sync():
     print("syncing time with RTC")
-    time.sleep(2)
+    time.sleep(.2)
     try:
-        os.system('sudo hwclock -s')
+        os.system('sudo hwclock -w')
+        time.sleep(.2)
+        print("Synced"),
+        #os.system('date')
     except RuntimeError:
         print("Failed to Sync time,Check RTC")
         
-    print(os.system('date;sudo hwclock -r'))
+    #print(os.system('date;sudo hwclock -r'))
 
 def currenttime():
-    print(os.system('sudo hwclock -r'))
-
+    t=str(datetime.now().time())
+    return t[:8]
+def currentdate():
+    return str(datetime.now().date())
