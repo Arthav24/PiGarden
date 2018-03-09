a199bcb737262aa93b7a86ac01ff66808bf8a62a
Shikhar Johri <30645315+johri002@users.noreply.github.com>
2018-03-09 14:05:39 +0530

    Update valve_relay_control.py

--- valve_relay_control.py
+++ valve_relay_control.py
@@ -4,24 +4,50 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-pins = [14,15,18] 
+pins = [14,15,18,23,24] 
 GPIO.setup(pins, GPIO.OUT)
 
-for pin in pins :
-	GPIO.output(pin,  GPIO.HIGH)
-	time.sleep(x)
-	#x depends on sensor module values x value 
-	#keeps on increasing until the soil sensor 
-	#value increases to an optimal value
+#relay attachments:
+#14 is garden valve
+#15 is seed plot valve
+#18 is bigger seed plot valve
+#23 is outdoor valve
+#24 is main pump motor
 
- 	if (str(pin)="14"):
- 		w="Garden"
- 	elif (str(pin)="15"):
+for components in pins[0:4]:
+	GPIO.output(pin, GPIO.LOW)
+	#opens all for valves
+time.sleep(5)
+GPIO.output(pins[5], GPIO.LOW)
+#turns on the main motor pump
+
+c=0
+while (c<=3):
+	avgsensorG
+	avgsensorSP
+	avgsensorBSP
+	avgsensorO
+	
+	if (str(avgsensorG)='1'):
+		w="Garden"
+		GPIO.output(pins[0], GPIO.HIGH)
+		c=c+1
+
+	elif (str(avgsensorSP)='1'):
 		w="seed plot"
-	else:
-		w="outdoors"
+		GPIO.output(pins[1], GPIO.HIGH)
+		c=c+1
 
-	print( "Watering the " + w +" is comleted" )
+	elif (str(avgsensorBSP)='1'):
+		w="bigger seed plot"
+		GPIO.output(pins[2], GPIO.HIGH)
+		c=c+1
+
+	elif (str(avgsensorO)='1'):
+		w="outdoors"
+		GPIO.output(pins[3], GPIO.HIGH)
+		c=c+1
+		
 
 GPIO.cleanup()
 print "Shutdown All relays"
